bff84a50aa1dbb16cf23b91704fe08a5b57459fe
Gong Chen <svenchen90@gmail.com>
2017-10-09 16:27:32 -0500

    1

--- GroupServer/public/js_gamma/gear.js
+++ GroupServer/public/js_gamma/gear.js
@@ -94,3 +94,11 @@ var callConfirm = function(title, text, actionConfirm, actionCancel){
 		}
 	});
 };
+/* 4. Random Color */
+var googleColorRandomPicker = function(){
+	var color_list = ['rgb(55, 141, 59)', 'rgb(65, 65, 65)', 'rgb(29, 135, 228)', 'rgb(119, 143, 155)', 'rgb(91, 106, 191)', 'rgb(125, 86, 193)', 'rgb(248, 167, 36)', 
+		'rgb(255, 111, 66)', 'rgb(235, 63, 121)', 'rgb(229, 57, 53)', 'rgb(140, 109, 98)'];
+	var random = Math.floor(Math.random() * (color_list.length));
+	//console.log(color_list[random]);
+	return color_list[random];
+};
